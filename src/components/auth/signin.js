69594b261f05b05d86dfc68afdb7300c720e57f9
Nicolai Organ <nicolaiorgan@Nicolais-MacBook-Pro-2.local>
2021-02-26 13:08:34 +0200

    sign in form

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import SignInForm from './signinForm';
+import PageTitle from '../pageTitle';
 
 class SignIn extends Component {
 
@@ -12,6 +13,7 @@ class SignIn extends Component {
         return (
             <div className='sign-in'>
                 <SignInForm onSubmit={this.onSubmit} className='sign-in__form' />
+                <PageTitle className='sign-in__page-title' title='Login' />
             </div>
         )
     }
